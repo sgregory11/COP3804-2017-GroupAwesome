e9f7414c4d11278d5ecb5190623a7297ab84ae5e
sgregory11 <sonjaygregory11@yahoo.com>
2017-10-04 21:55:09 -0400

    9:55 Update

--- NetBeansProjects/AcadAdminSystem_Phase1/src/acadadminsystem_phase1/CourseGrade.java
+++ NetBeansProjects/AcadAdminSystem_Phase1/src/acadadminsystem_phase1/CourseGrade.java
@@ -1,9 +1,88 @@
-/**
- *
- * @author Sonjay
- */
 package acadadminsystem_phase1;
 
-public class CourseGrade {
+import java.util.Objects;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David Santana
+ */
+class CourseGrade {
+    private Course courseTaken;
+    private String letterGrade;
+    
+    /**
+     * Normal Constructor
+     * @param cTaken
+     * @param lGrade 
+     */
+    public CourseGrade(Course cTaken, String lGrade){
+        courseTaken = cTaken;
+        letterGrade = lGrade;
+    }
+    
+    /**
+     * Copy Constructor
+     * @param cGrade 
+     */
+    public CourseGrade(CourseGrade cGrade){
+        courseTaken = cGrade.getCourseTaken();
+        letterGrade = cGrade.getLetterGrade();
+    }
+
+    /**
+     * Returns instance variables
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return "CourseGrade{" + "courseTaken=" + courseTaken
+                + ", letterGrade=" + letterGrade + '}';
+    }
+
+    /**
+     * 
+     * @param obj
+     * @return 
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof CourseGrade)){
+            return false;
+        }
+        
+        CourseGrade cGrade = (CourseGrade) obj;
+        
+        return courseTaken.equals(cGrade);
+    }
+
+    /**
+     * Getters and Setters
+     * @return 
+     */
+    
+    public Course getCourseTaken() {
+        return courseTaken;
+    }
+
+    public String getLetterGrade() {
+        return letterGrade;
+    }
+
+    public void setCourseTaken(Course courseTaken) {
+        this.courseTaken = courseTaken;
+    }
+
+    public void setLetterGrade(String letterGrade) {
+        this.letterGrade = letterGrade;
+    }
+    
+    
+    
     
 }
--- NetBeansProjects/AcadAdminSystem_Phase1/src/acadadminsystem_phase1/Student.java
+++ NetBeansProjects/AcadAdminSystem_Phase1/src/acadadminsystem_phase1/Student.java
@@ -1,9 +1,129 @@
-/**
- *
- * @author Sonjay
- */
 package acadadminsystem_phase1;
 
-public class Student {
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author David Santana
+ */
+public class Student extends Person {
+
+    private int studentID;
+    private String dateOfAdmission;
+    private String major;
+    private ArrayList<CourseGrade> coursesTaken;
+
+    /**
+     * Normal Constructor
+     *
+     * @param id Student ID
+     * @param dateAdd Date of Admission
+     * @param maj Major
+     * @param cTaken Courses Taken
+     */
+    public Student(String fName, String lName, int id, String dateAdd, String maj, ArrayList<CourseGrade> cTaken) {
+        super(fName, lName);
+        studentID = id;
+        dateOfAdmission = dateAdd;
+        major = maj;
+        coursesTaken = cTaken;
+    }
+
+    /**
+     * Copy Constructor
+     *
+     * @param stud
+     */
+    public Student(Student stud) {
+        /*super con*/
+        super()
+        studentID = stud.getStudentID();
+        dateOfAdmission = stud.getDateOfAdmission();
+        major = stud.getMajor();
+        coursesTaken = stud.getCoursesTaken();
+    }
+
+    /**
+     * Creates string with all courses taken
+     * @return 
+     */
+    private String allCoursesTaken() {
+        String temp = "";
+        for (int i = 0; i < coursesTaken.size(); i++) {
+            temp += coursesTaken.get(i) + ", ";
+        }
+        return temp;
+    }
     
+    /**
+     * Returns variables
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return "Student{" + "studentID="
+                + studentID + ", dateOfAdmission=" + dateOfAdmission
+                + ", major=" + major
+                + ", coursesTaken=" + allCoursesTaken() + '}';
+    }
+    
+    /**
+     * Equals Method
+     * @param obj
+     * @return 
+     */
+    @Override
+    public boolean equals(Object obj){
+        if(!(obj instanceof Student)){
+            return false;
+        }
+        
+        Student std = (Student) obj;
+        
+        return getFirstName().equals(std.getFirstName())
+               && getLastName().equals(std.getLastName())
+               && dateOfAdmission.equals(std.getDateOfAdmission());
+    }
+    
+    /**
+     * Getters and Setters
+     */
+    
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getDateOfAdmission() {
+        return dateOfAdmission;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public ArrayList<CourseGrade> getCoursesTaken() {
+        return coursesTaken;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setDateOfAdmission(String dateOfAdmission) {
+        this.dateOfAdmission = dateOfAdmission;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setCoursesTaken(ArrayList<CourseGrade> coursesTaken) {
+        this.coursesTaken = coursesTaken;
+    }
+
 }
